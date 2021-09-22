d6fee0f3aca043cbe06b29429e5f2220cd1e316e
dwjclark11 <dwjclark1@hotmail.com>
2021-09-22 12:24:32 -0400

    Remove Unused assets

---
+++ ZeldaClone/Assets/Levels/sword_dungeon.lua
@@ -0,0 +1,41 @@
+----------------------------------------------------
+-- All the Entities that are in the Over World
+----------------------------------------------------
+level_data = 
+	{
+		collectables = 
+			{
+				[1] = 
+				{
+					
+				}			
+			},
+		npc = 
+		{
+			[1] = 
+			{
+				group = "npc",
+				components = 
+				{
+					transform = 
+					{
+						position = { x = 0, y = 0 },
+						scale = { x = 4, y = 4 },
+						rotation = 0.0
+					},
+					sprite = 
+					{
+						assetID = "npc_sprites",
+						width = 16,
+						height = 16, 
+						layer = 1,
+						fixed = false,
+						src_rect_x = 0,
+						src_rect_y = 0
+					},
+				}
+			}					
+		}
+	}
+	
+
