7244917d601944215ecb213fc960b40bf7eaf865
Dustin Clark <dwjclark1@hotmail.com>
2023-05-12 10:47:37 -0400

    Removed added systems in main

--- ZeldaClone/src/main.cpp
+++ ZeldaClone/src/main.cpp
@@ -1,21 +1,12 @@
-#define SDL_MAIN_HANDLED
-#define main SDL_main
+#define SDL_MAIN_HANDLED 1
+
 
 #include "Game/Game.h"
-#include "Systems/CameraMovementSystem.h"
-#include "Systems/SoundFXSystem.h"
+
 
 int main(int argc, char* argv[])
 {
-	if (!Registry::Instance().HasSystem<SoundFXSystem>()) 
-		Registry::Instance().AddSystem<SoundFXSystem>();
-	if (!Registry::Instance().HasSystem<MusicPlayerSystem>()) 
-		Registry::Instance().AddSystem<MusicPlayerSystem>();
-	if (!Registry::Instance().HasSystem<CameraMovementSystem>()) 
-		Registry::Instance().AddSystem<CameraMovementSystem>();
 
-	// Turn music volume down
-	Mix_VolumeMusic(10);
 
 	// Run the game Instance--> There is a loop inside this
 	Game::Instance().Run();
